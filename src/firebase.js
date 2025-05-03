bb01a354601d42d113ecfbbef9fca174afb6a05b
Bobby-Krishnan <Abhishek.v.krishnan@gmail.com>
2025-05-03 05:20:06 -0400

    added web client info

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCAbGUU4A6HIJhCafiCnocigKIerWpwSLE",
+  authDomain: "thinkfast-df319.firebaseapp.com",
+  projectId: "thinkfast-df319",
+  storageBucket: "thinkfast-df319.appspot.com",
+  messagingSenderId: "1094438522843",
+  appId: "1:1094438522843:web:6480a03d8b6975006861fe",
+  measurementId: "G-PZGETJ88QT"
 };
 
 const app = initializeApp(firebaseConfig);
