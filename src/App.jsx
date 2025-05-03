1380db1ac788584ac9b52853888cbbbe5136df10
Bobby Krishnan <bobbykrishnan@Bobbys-MacBook-Pro.local>
2025-05-02 21:36:56 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>ThinkFast: Mental Math Trainer</h1>
+      <p>Let's build your brain </p>
+    </div>
+  );
+}
+
+export default App;
